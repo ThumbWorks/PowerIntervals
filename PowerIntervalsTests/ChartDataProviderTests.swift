1f3ca631dae6249c0437e5e00cbf2c952ea8864f
Roderic Campbell <roderic@gmail.com>
2017-01-14 22:09:01 -0800

    A few basic tests for the ChartDataProvider

--- PowerIntervalsTests/ChartDataProviderTests.swift
+++ PowerIntervalsTests/ChartDataProviderTests.swift
@@ -7,11 +7,34 @@
 //
 
 import XCTest
+@testable import PowerIntervals
 
-class ChartDataProvider: XCTestCase {
+class ChartDataProviderTests: XCTestCase {
+    let powerZones = PowerZone()
+    var dataPoints = [WorkoutDataPoint]()
     
     override func setUp() {
         super.setUp()
+        
+        let dataPoint1 = WorkoutDataPoint()
+        dataPoint1.time = 0
+        dataPoint1.watts = 0
+        dataPoint1.deviceID = "dummy"
+        
+        let dataPoint2 = WorkoutDataPoint()
+        dataPoint2.time = 100
+        dataPoint2.watts = 1001
+        dataPoint2.deviceID = "dummy"
+        dataPoints = [dataPoint1, dataPoint2]
+        
+        powerZones.neuromuscular = 1000
+        powerZones.anaerobicCapacity = 759
+        powerZones.VO2Max = 600
+        powerZones.lactateThreshold = 500
+        powerZones.tempo = 350
+        powerZones.endurance = 200
+        powerZones.activeRecovery = 100
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -20,10 +43,25 @@ class ChartDataProvider: XCTestCase {
         super.tearDown()
     }
     
-    func testXEqualsY() {
+    func testVerticalHeight() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        ChartDataProvider
+        let chartDataProvider = ChartDataProvider()
+        chartDataProvider.dataPoints = dataPoints
+        chartDataProvider.zones = powerZones
+        
+        let topLine1 = chartDataProvider.lineChartView(nil, verticalValueForHorizontalIndex: 0, atLineIndex: 0)
+        XCTAssertTrue(topLine1 == 1001)
+        
+        let topLine2 = chartDataProvider.lineChartView(nil, verticalValueForHorizontalIndex: 1, atLineIndex: 0)
+        XCTAssertTrue(topLine2 == 1001)
+        
+        let wattsValue0 = chartDataProvider.lineChartView(nil, verticalValueForHorizontalIndex: 0, atLineIndex: 7)
+        XCTAssertTrue(wattsValue0 == 0)
+        
+        let wattsValue1 = chartDataProvider.lineChartView(nil, verticalValueForHorizontalIndex: 1, atLineIndex: 7)
+        XCTAssertTrue(wattsValue1 == 1001)
+
     }
     
     func testPerformanceExample() {
